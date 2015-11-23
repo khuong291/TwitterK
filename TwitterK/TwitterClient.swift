466ecb3612c6b573c30a7af1a3ae55e21ffb1033
Khuong Pham <dkhuong291@gmail.com>
2015-11-24 00:29:27 +0700

    Initial commit

---
+++ TwitterK/TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  TwitterK
+//
+//  Created by Khuong Pham on 11/24/15.
+//  Copyright © 2015 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class TwitterClient: BDBOAuth1RequestOperationManager {
+
+}

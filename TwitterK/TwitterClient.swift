e35d40355fe25a3c3d536a7dd2faab68accaa554
Khuong Pham <dkhuong291@gmail.com>
2015-11-24 00:46:29 +0700

    Create TwitterClient.swift

--- TwitterK/TwitterClient.swift
+++ TwitterK/TwitterClient.swift
@@ -8,6 +8,17 @@
 
 import UIKit
 
+let twitterConsumerKey = "Gcg7jzE4qiKzjX0f4x0BoC4PU"
+let twitterConsumerSecret = "l24v7fw7fQUiGVNpcrOAf4ISRXxdhi4BBOpt6xxBuP1CL5buvM"
+let twitterBaseURL = NSURL(string: "http://api.twitter.com")
+
 class TwitterClient: BDBOAuth1RequestOperationManager {
 
+    class var sharedInstance: TwitterClient {
+        struct Static {
+            static let instance = TwitterClient(baseURL: twitterBaseURL, consumerKey: twitterConsumerKey, consumerSecret: twitterConsumerSecret)
+        }
+
+        return Static.instance
+    }
 }
